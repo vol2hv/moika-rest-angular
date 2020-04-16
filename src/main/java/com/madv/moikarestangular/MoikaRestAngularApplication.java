4149ff7507535b3f4db27f5a1aeb69c9df08a8c1
vol2hv <vol2hv@yandex.ru>
2020-04-16 16:06:12 +0300

    backend

---
+++ src/main/java/com/madv/moikarestangular/MoikaRestAngularApplication.java
@@ -0,0 +1,13 @@
+package com.madv.moikarestangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoikaRestAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoikaRestAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/madv/moikarestangular/MoikaRestAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madv.moikarestangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoikaRestAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
